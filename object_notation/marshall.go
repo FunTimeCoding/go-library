3359298b3ddca02dfbd747e25a553ee304dd5ff3
Alexander Reitzel <funtimecoding@gmail.com>
2023-06-27 14:03:25 +0200

    shorter name

--- object_notation/marshall.go
+++ object_notation/marshall.go
@@ -5,7 +5,7 @@ import (
 	"github.com/funtimecoding/go-library/errors"
 )
 
-func MarshallAny(a any) []byte {
+func Marshall(a any) []byte {
 	result, e := json.Marshal(a)
 	errors.PanicOnError(e)
 

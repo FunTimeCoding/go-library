52d94646b58e3a4500b830bce1e54bad73e5203f
Alexander Reitzel <funtimecoding@gmail.com>
2023-08-05 19:07:30 +0200

    see if there is something if its not a strict error type, and make it one

--- errors/from.go
+++ errors/from.go
@@ -1,9 +1,20 @@
 package errors
 
+import (
+	"errors"
+	"fmt"
+)
+
 func From(a any) error {
 	switch cast := a.(type) {
 	case error:
 		return cast
+	default:
+		v := fmt.Sprintf("%v", a)
+
+		if v != "" {
+			return errors.New(fmt.Sprintf("non-error: %s", v))
+		}
 	}
 
 	return nil

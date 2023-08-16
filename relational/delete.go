fa1b19836e50d9d832e580b383a06bc0b3e66343
Alexander Reitzel <funtimecoding@gmail.com>
2023-08-17 00:05:53 +0200

    Delete uses DeleteWhere

--- relational/delete.go
+++ relational/delete.go
@@ -1,7 +1,5 @@
 package relational
 
-import "github.com/funtimecoding/go-library/errors"
-
 func (d *Database) Delete(a any) {
-	errors.PanicOnError(d.mapper.Delete(a, "TRUE").Error)
+	d.DeleteWhere(a, "TRUE")
 }

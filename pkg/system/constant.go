ba8e45b262c0ffa6610927b7cd7834bae7b3e78d
Alexander Reitzel <funtimecoding@gmail.com>
2023-08-28 18:09:41 +0200

    linux and darwin constants

--- pkg/system/constant.go
+++ pkg/system/constant.go
@@ -1,5 +1,8 @@
 package system
 
+const Linux = "linux"
+const Darwin = "Darwin"
+
 const NullPhysicalAddressString = "00:00:00:00:00:00"
 
 var NullPhysicalAddress = PhysicalAddress(NullPhysicalAddressString)

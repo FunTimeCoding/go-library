b7adc05c9cbe1a80a8d2886f60a6c35d98a42df8
Alexander Reitzel <funtimecoding@gmail.com>
2023-08-31 13:11:53 +0200

    architecture constants

--- pkg/system/constant.go
+++ pkg/system/constant.go
@@ -1,8 +1,13 @@
 package system
 
-const Linux = "linux"
-const Darwin = "darwin"
+const (
+	NullPhysicalAddressString = "00:00:00:00:00:00"
 
-const NullPhysicalAddressString = "00:00:00:00:00:00"
+	Linux  = "linux"
+	Darwin = "darwin"
+
+	AMD64 = "amd64"
+	ARM64 = "arm64"
+)
 
 var NullPhysicalAddress = PhysicalAddress(NullPhysicalAddressString)

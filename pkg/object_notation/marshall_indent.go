bafe552b9cdee20e15122c971ef9e02238b53a3c
Alexander Reitzel <funtimecoding@gmail.com>
2023-10-23 17:54:33 +0200

    both bytes and string are common

--- pkg/object_notation/marshall_indent.go
+++ pkg/object_notation/marshall_indent.go
@@ -1,13 +1,5 @@
 package object_notation
 
-import (
-	"encoding/json"
-	"github.com/funtimecoding/go-library/pkg/errors"
-)
-
 func MarshallIndent(a any) string {
-	result, e := json.MarshalIndent(a, "", "\t")
-	errors.PanicOnError(e)
-
-	return string(result)
+	return string(MarshallIndentBytes(a))
 }

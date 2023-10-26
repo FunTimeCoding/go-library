a9ec54398a77399c1ba3c988f8590e067ed9d7e2
Alexander Reitzel <funtimecoding@gmail.com>
2023-10-26 17:11:35 +0200

    files by extension

---
+++ pkg/system/find_files_by_extension.go
@@ -0,0 +1,33 @@
+package system
+
+import (
+	"github.com/funtimecoding/go-library/pkg/errors"
+	"os"
+	"path/filepath"
+)
+
+func FindFilesByExtensions(
+	root string,
+	extension string,
+) []string {
+	var result []string
+
+	errors.PanicOnError(
+		filepath.WalkDir(
+			root,
+			func(path string, d os.DirEntry, e error) error {
+				if e != nil {
+					return e
+				}
+
+				if !d.IsDir() && filepath.Ext(path) == extension {
+					result = append(result, path)
+				}
+
+				return nil
+			},
+		),
+	)
+
+	return result
+}

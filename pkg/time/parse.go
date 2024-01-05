21a75ad88a503272d84d960230bc258a8c0a629f
Alexander Reitzel <funtimecoding@gmail.com>
2024-01-05 17:34:12 +0100

    time parse

---
+++ pkg/time/parse.go
@@ -0,0 +1,13 @@
+package time
+
+import (
+	"github.com/funtimecoding/go-library/pkg/errors"
+	"time"
+)
+
+func Parse(layout string, s string) time.Time {
+	result, e := time.Parse(layout, s)
+	errors.PanicOnError(e)
+
+	return result
+}

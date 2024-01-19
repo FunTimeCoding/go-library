d9e18cda160d5155d6b762a8a86e1b0c698266f7
Alexander Reitzel <funtimecoding@gmail.com>
2024-01-19 14:22:22 +0100

    path configurable

--- pkg/git/modified_files.go
+++ pkg/git/modified_files.go
@@ -1,14 +1,11 @@
 package git
 
-import (
-	"github.com/funtimecoding/go-library/pkg/system"
-	"github.com/go-git/go-git/v5"
-)
+import "github.com/go-git/go-git/v5"
 
-func ModifiedFiles() []string {
+func ModifiedFiles(path string) []string {
 	var result []string
 
-	for file, status := range Status(system.WorkingDirectory()) {
+	for file, status := range Status(path) {
 		// Staging: staged changes
 		// Worktree: non-staged changes
 		if status.Staging == git.Modified || status.Worktree == git.Modified {

2c7186fa938671f6c061cff8defd805f0c048286
Alexander Reitzel <funtimecoding@gmail.com>
2024-03-14 17:30:59 +0100

    include proxied golang image

--- pkg/project/replace_go_from_version.go
+++ pkg/project/replace_go_from_version.go
@@ -4,5 +4,9 @@ func ReplaceGoFromVersion(
 	content string,
 	v string,
 ) string {
-	return ReplaceVersionByPrefix(content, v, `FROM golang:`)
+	return ReplaceVersionByPrefix(
+		content,
+		v,
+		`FROM ([a-z\.\/]+)?golang:`,
+	)
 }

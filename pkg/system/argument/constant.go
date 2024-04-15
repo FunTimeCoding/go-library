c5162d7f4beab1c699f9d8f6597d65aacc3c28f7
Alexander Reitzel <funtimecoding@gmail.com>
2024-04-15 17:49:09 +0200

    reusable arguments

---
+++ pkg/system/argument/constant.go
@@ -0,0 +1,23 @@
+package argument
+
+const (
+	Branch     = "branch"
+	Executable = "executable"
+	Header     = "header"
+	Host       = "host"
+	Locator    = "locator"
+	Message    = "message"
+	Name       = "name"
+	Output     = "output"
+	Owner      = "owner"
+	Package    = "package"
+	Path       = "path"
+	Project    = "project"
+	Replace    = "replace"
+	Repository = "repository"
+	Tag        = "tag"
+	Template   = "template"
+	Token      = "token"
+	Verbose    = "verbose"
+	Version    = "version"
+)
---
+++ pkg/system/argument/constant_test.go
@@ -0,0 +1,28 @@
+package argument
+
+import (
+	"github.com/funtimecoding/go-library/pkg/assert"
+	"testing"
+)
+
+func TestConstant(t *testing.T) {
+	assert.String(t, "branch", Branch)
+	assert.String(t, "executable", Executable)
+	assert.String(t, "header", Header)
+	assert.String(t, "host", Host)
+	assert.String(t, "locator", Locator)
+	assert.String(t, "message", Message)
+	assert.String(t, "name", Name)
+	assert.String(t, "output", Output)
+	assert.String(t, "owner", Owner)
+	assert.String(t, "package", Package)
+	assert.String(t, "path", Path)
+	assert.String(t, "project", Project)
+	assert.String(t, "replace", Replace)
+	assert.String(t, "repository", Repository)
+	assert.String(t, "tag", Tag)
+	assert.String(t, "template", Template)
+	assert.String(t, "token", Token)
+	assert.String(t, "verbose", Verbose)
+	assert.String(t, "version", Version)
+}

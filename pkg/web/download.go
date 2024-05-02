32d9a008ca5cd4c1366ed79a599d4420e5df4b25
Alexander Reitzel <funtimecoding@gmail.com>
2024-05-02 17:17:08 +0200

    first get, then create file, to avoid an empty file being created

--- pkg/web/download.go
+++ pkg/web/download.go
@@ -9,16 +9,16 @@ func Download(
 	locator string,
 	output string,
 ) {
-	f := system.Create(output)
-	defer func() {
-		errors.LogClose(f)
-	}()
-
 	r := BasicGet(locator)
 	defer func() {
 		errors.LogClose(r.Body)
 	}()
 
 	errors.PanicStatus(r)
+	f := system.Create(output)
+	defer func() {
+		errors.LogClose(f)
+	}()
+
 	system.Copy(r.Body, f)
 }

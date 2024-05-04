6f198c51b09beea23d8dc34d68240291542ff1d3
Alexander Reitzel <funtimecoding@gmail.com>
2024-05-04 14:03:56 +0200

    empty check

---
+++ pkg/text/multi_line/empty.go
@@ -0,0 +1,5 @@
+package multi_line
+
+func (m *MultiLine) Empty() bool {
+	return m.Count() == 0
+}
--- pkg/text/multi_line/multi_line_test.go
+++ pkg/text/multi_line/multi_line_test.go
@@ -7,7 +7,9 @@ import (
 
 func TestMultiLine(t *testing.T) {
 	l := New()
+	assert.Boolean(t, true, l.Empty())
 	l.Add("a")
+	assert.Boolean(t, false, l.Empty())
 	l.Add("b")
 	assert.String(t, "a\nb", l.Format())
 }

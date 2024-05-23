ccb5450b0bc49c6f4b6e505b20270bb07b9817aa
Alexander Reitzel <funtimecoding@gmail.com>
2024-05-23 11:19:30 +0200

    do not panic if time cannot be parsed, set zero time

--- pkg/linux/systemd/helper/parse_timestamp.go
+++ pkg/linux/systemd/helper/parse_timestamp.go
@@ -1,14 +1,16 @@
 package helper
 
 import (
-	"github.com/funtimecoding/go-library/pkg/errors"
 	"github.com/funtimecoding/go-library/pkg/linux/systemd/constant"
 	"time"
 )
 
 func ParseTimestamp(s string) time.Time {
 	result, e := time.Parse(constant.DateTime, s)
-	errors.PanicOnError(e)
+
+	if e != nil {
+		return time.Time{}
+	}
 
 	return result
 }

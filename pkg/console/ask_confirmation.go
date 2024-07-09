2ea1a4dde70315b8d3f4972bb4e6c8ad1c7309e1
Alexander Reitzel <funtimecoding@gmail.com>
2024-07-09 17:41:16 +0200

    ask confirmation

---
+++ pkg/console/ask_confirmation.go
@@ -0,0 +1,26 @@
+package console
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/funtimecoding/go-library/pkg/errors"
+	"os"
+	"strings"
+)
+
+func AskConfirmation(s string) bool {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Printf("%s [y/N]: ", s)
+		response, e := reader.ReadString('\n')
+		errors.PanicOnError(e)
+		response = strings.ToLower(strings.TrimSpace(response))
+
+		if response == "y" || response == "yes" {
+			return true
+		}
+
+		return false
+	}
+}

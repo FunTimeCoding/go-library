a8557f0b89b0bdbb1f5b2ace348a68532f34351d
Alexander Reitzel <funtimecoding@gmail.com>
2024-08-08 11:09:13 +0200

    catch error

--- pkg/sound/play.go
+++ pkg/sound/play.go
@@ -11,17 +11,23 @@ import (
 func Play(
 	path string,
 	volume float64,
+	verbose bool,
 ) {
 	switch p := runtime.GOOS; p {
 	case constant.Linux:
 		panic("not implemented")
 	case constant.Darwin:
-		system.Run(
+		result, e := system.RunError(
 			Afplay,
 			VolumeArgument,
 			fmt.Sprintf("%.2f", volume),
 			path,
 		)
+
+		if e != nil && verbose {
+			fmt.Printf("Sound error: %s\n", e)
+			fmt.Printf("Output: %s\n", result)
+		}
 	default:
 		unexpected.String(p)
 	}

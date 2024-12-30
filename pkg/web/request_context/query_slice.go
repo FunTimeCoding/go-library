1b6243be6446e61b8065770668dfb3583cb21862
Alexander Reitzel <funtimecoding@gmail.com>
2024-12-30 01:06:50 +0100

    lint

--- pkg/web/request_context/query_slice.go
+++ pkg/web/request_context/query_slice.go
@@ -1,8 +1,6 @@
 package request_context
 
-import (
-	"github.com/funtimecoding/go-library/pkg/strings/split"
-)
+import "github.com/funtimecoding/go-library/pkg/strings/split"
 
 func (c *Context) QuerySlice(k string) []string {
 	return split.Comma(c.Query(k))

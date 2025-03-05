22a4c4c9575e824589dfaa83a7ede72bcfe47c76
Alexander Reitzel <funtimecoding@gmail.com>
2025-03-05 15:25:22 +0100

    compare

---
+++ pkg/gitlab/compare.go
@@ -0,0 +1,23 @@
+package gitlab
+
+import (
+	"github.com/funtimecoding/go-library/pkg/errors"
+	"github.com/funtimecoding/go-library/pkg/ptr"
+	gitlab "gitlab.com/gitlab-org/api/client-go"
+)
+
+func (c *Client) Compare(
+	project int,
+	from, to string,
+) *gitlab.Compare {
+	result, _, e := c.client.Repositories.Compare(
+		project,
+		&gitlab.CompareOptions{
+			From: ptr.To(from),
+			To:   ptr.To(to),
+		},
+	)
+	errors.PanicOnError(e)
+
+	return result
+}

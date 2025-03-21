587776a6050692ec57d33708baaba2d3f73c76f8
Alexander Reitzel <funtimecoding@gmail.com>
2025-03-21 16:34:29 +0100

    constants

--- pkg/prometheus/alertmanager/constant/alert_test.go
+++ pkg/prometheus/alertmanager/constant/alert_test.go
@@ -8,6 +8,7 @@ import (
 func TestAlert(t *testing.T) {
 	assert.String(t, "Check", Check)
 	assert.String(t, "Cluster", Cluster)
+	assert.String(t, "Configuration", Configuration)
 	assert.String(t, "Connection", Connection)
 	assert.String(t, "Container", Container)
 	assert.String(t, "Database", Database)
@@ -16,6 +17,7 @@ func TestAlert(t *testing.T) {
 	assert.String(t, "Fan", Fan)
 	assert.String(t, "Hardware", Hardware)
 	assert.String(t, "Latency", Latency)
+	assert.String(t, "Limit", Limit)
 	assert.String(t, "Memory", Memory)
 	assert.String(t, "Node", Node)
 	assert.String(t, "Pod", Pod)
@@ -24,10 +26,12 @@ func TestAlert(t *testing.T) {
 	assert.String(t, "Service", Service)
 	assert.String(t, "Volume", Volume)
 
+	assert.String(t, "Bad", Bad)
 	assert.String(t, "Broken", Broken)
 	assert.String(t, "Corrupt", Corrupt)
 	assert.String(t, "Disconnected", Disconnected)
 	assert.String(t, "Down", Down)
+	assert.String(t, "Exceeded", Exceeded)
 	assert.String(t, "High", High)
 	assert.String(t, "Lag", Lag)
 	assert.String(t, "Load", Load)

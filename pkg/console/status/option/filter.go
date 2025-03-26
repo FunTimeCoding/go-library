9c3d2caab52d16aa34676aaba0584621db277de5
Alexander Reitzel <funtimecoding@gmail.com>
2025-03-26 17:41:33 +0100

    return self

--- pkg/console/status/option/filter.go
+++ pkg/console/status/option/filter.go
@@ -5,6 +5,8 @@ import "github.com/funtimecoding/go-library/pkg/console/status/option/pair"
 func (f *Format) Filter(
 	k string,
 	v string,
-) {
+) *Format {
 	f.Filters = append(f.Filters, pair.New(k, v))
+
+	return f
 }

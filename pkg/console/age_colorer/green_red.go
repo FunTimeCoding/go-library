838cbc231e7969a6e7eedf07fb40b4cabd9ad4fd
Alexander Reitzel <funtimecoding@gmail.com>
2025-03-26 12:20:10 +0100

    softer colors

--- pkg/console/age_colorer/green_red.go
+++ pkg/console/age_colorer/green_red.go
@@ -3,5 +3,5 @@ package age_colorer
 import "github.com/funtimecoding/go-library/pkg/face"
 
 func GreenRed(v ...face.AgeColorable) *Colorer {
-	return Dynamic("#00ff00", "#ff0000", v...)
+	return Dynamic("#00aa00", "#aa0000", v...)
 }
--- pkg/console/score_colorer/green_red.go
+++ pkg/console/score_colorer/green_red.go
@@ -3,5 +3,5 @@ package score_colorer
 import "github.com/funtimecoding/go-library/pkg/face"
 
 func GreenRed(v ...face.ScoreColorable) *Colorer {
-	return Dynamic("#00ff00", "#ff0000", v...)
+	return Dynamic("#00aa00", "#aa0000", v...)
 }

8cd1368158704da3d744172b4b639511ccdf26e0
Alexander Reitzel <funtimecoding@gmail.com>
2025-04-02 18:04:37 +0200

    format

--- pkg/ollama/generate/format.go
+++ pkg/ollama/generate/format.go
@@ -5,8 +5,8 @@ import "fmt"
 func (g *Generate) Print() {
 	fmt.Printf("Total: %dms\n", g.Total)
 	fmt.Printf("  Load: %dms\n", g.Load)
-	fmt.Printf("Prompt tokens: %.0f/s\n", g.PromptTokens)
-	fmt.Printf("  Evaluation: %dms\n", g.PromptEvaluation)
-	fmt.Printf("Tokens: %.0f/s\n", g.Tokens)
+	fmt.Printf("  Prompt evaluation: %dms\n", g.PromptEvaluation)
+	fmt.Printf("    Tokens/s: %.0f\n", g.PromptTokens)
 	fmt.Printf("  Evaluation: %dms\n", g.Evaluation)
+	fmt.Printf("    Tokens/s: %.0f\n", g.Tokens)
 }

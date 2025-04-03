2f711fb3fb76bb6e388aeca46d32b25103196d7b
Alexander Reitzel <funtimecoding@gmail.com>
2025-04-03 10:46:02 +0200

    todo

--- cmd/example/confluence/main.go
+++ cmd/example/confluence/main.go
@@ -4,6 +4,8 @@ import "github.com/funtimecoding/go-library/pkg/atlassian/confluence/example"
 
 func main() {
 	if true {
+		// TODO: Load all pages in a space and save as JSON, with markdown text
+		// TODO: Load all sub-pages of a page and save as JSON, with markdown text
 		example.Page()
 	}
 

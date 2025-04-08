9c0b5658ab3020f5d9575ef99ce1b6fcd8ac42fd
Alexander Reitzel <funtimecoding@gmail.com>
2025-04-08 16:03:59 +0200

    by identifier

--- pkg/atlassian/opsgenie/alert/has_responder_team.go
+++ pkg/atlassian/opsgenie/alert/has_responder_team.go
@@ -2,9 +2,9 @@ package alert
 
 import rawAlert "github.com/opsgenie/opsgenie-go-sdk-v2/alert"
 
-func (a *Alert) HasResponderTeam(name string) bool {
+func (a *Alert) HasResponderTeam(identifier string) bool {
 	for _, r := range a.Responders {
-		if r.Type == rawAlert.TeamResponder && r.Name == name {
+		if r.Type == rawAlert.TeamResponder && r.Id == identifier {
 			return true
 		}
 	}
--- pkg/atlassian/opsgenie/alert/has_responder_user.go
+++ pkg/atlassian/opsgenie/alert/has_responder_user.go
@@ -2,9 +2,9 @@ package alert
 
 import rawAlert "github.com/opsgenie/opsgenie-go-sdk-v2/alert"
 
-func (a *Alert) HasResponderUser(name string) bool {
+func (a *Alert) HasResponderUser(identifier string) bool {
 	for _, r := range a.Responders {
-		if r.Type == rawAlert.UserResponder && r.Name == name {
+		if r.Type == rawAlert.UserResponder && r.Id == identifier {
 			return true
 		}
 	}

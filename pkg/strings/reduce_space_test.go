711bd5b7e42f5723679def296939299218be9514
Alexander Reitzel <funtimecoding@gmail.com>
2025-05-13 12:40:54 +0200

    more reduce space tests

--- pkg/strings/reduce_space_test.go
+++ pkg/strings/reduce_space_test.go
@@ -7,4 +7,6 @@ import (
 
 func TestReduceSpace(t *testing.T) {
 	assert.String(t, " ", ReduceSpace("  "))
+	assert.String(t, "a b", ReduceSpace("a  b"))
+	assert.String(t, "a b c", ReduceSpace("a  b   c"))
 }

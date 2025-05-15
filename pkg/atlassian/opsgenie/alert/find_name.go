d5444f6c9c3a1b6b804806e267acf92dd94ba308
Alexander Reitzel <funtimecoding@gmail.com>
2025-05-15 13:19:04 +0200

    trim space

--- pkg/atlassian/opsgenie/alert/find_name.go
+++ pkg/atlassian/opsgenie/alert/find_name.go
@@ -3,13 +3,13 @@ package alert
 import (
 	"fmt"
 	"github.com/funtimecoding/go-library/pkg/prometheus/alertmanager/constant"
+	"strings"
 )
 
 func (a *Alert) findName() string {
-	var result string
-	var details map[string]string
 	var message string
 	var description string
+	var details map[string]string
 
 	if a.RawList != nil {
 		// details and description not available
@@ -18,29 +18,29 @@ func (a *Alert) findName() string {
 
 	if a.RawDetail != nil {
 		message = a.RawDetail.Message
-		details = a.RawDetail.Details
 		description = a.RawDetail.Description
+		details = a.RawDetail.Details
 	}
 
 	if n, okay := details[constant.AlertnameLabel]; okay {
 		return a.shortenAlert(n)
 	}
 
-	result = a.shortenAlert(message)
+	result := a.shortenAlert(message)
 
 	if result == UnknownName {
 		result = a.nameFromDescription(description)
+
+		if result == UnknownName {
+			if a.RawList != nil {
+				fmt.Printf("Unknown name (simple): %+v\n", a.RawList)
+			}
+
+			if a.RawDetail != nil {
+				fmt.Printf("Unknown name (full): %+v\n", a.RawDetail)
+			}
+		}
 	}
 
-	if result == UnknownName {
-		if a.RawList != nil {
-			fmt.Printf("Unknown name (simple): %+v\n", a.RawList)
-		}
-
-		if a.RawDetail != nil {
-			fmt.Printf("Unknown name (full): %+v\n", a.RawDetail)
-		}
-	}
-
-	return result
+	return strings.TrimSpace(result)
 }

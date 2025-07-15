386292c583d9f1ddc9445e4e047d46d075e86e54
Alexander Reitzel <funtimecoding@gmail.com>
2025-07-15 09:56:06 +0200

    comment

--- pkg/atlassian/opsgenie/alert/format_name.go
+++ pkg/atlassian/opsgenie/alert/format_name.go
@@ -9,6 +9,7 @@ func (a *Alert) formatName(f *option.Format) string {
 	result := a.Name
 
 	if f.UseColor {
+		// Not meant to be cyan, because entity and category are cyan
 		result = console.Yellow("%s", result)
 	}
 

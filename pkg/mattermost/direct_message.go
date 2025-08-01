2f5e2e84296065356303f32bc99c24c7b964cbc7
Alexander Reitzel <funtimecoding@gmail.com>
2025-08-01 14:12:35 +0200

    direct message

---
+++ pkg/mattermost/direct_message.go
@@ -0,0 +1,13 @@
+package mattermost
+
+import "github.com/mattermost/mattermost/server/public/model"
+
+func (c *Client) DirectMessage(
+	u *model.User,
+	text string,
+) *model.Post {
+	channel, r, e := c.client.CreateDirectChannel(c.context, c.Me().Id, u.Id)
+	panicOnError(e, r)
+
+	return c.PostSimple(channel, text)
+}

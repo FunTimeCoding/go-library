8be562bd0c1b0ccfedfd20510e034c36bbf2bec2
Alexander Reitzel <funtimecoding@gmail.com>
2025-09-15 15:35:07 +0200

    trivy

--- pkg/kubernetes/example/job.go
+++ pkg/kubernetes/example/job.go
@@ -6,6 +6,8 @@ import (
 )
 
 const (
+	TrivyNamespace    = "trivy"
+	TrivyCron         = "trivy"
 	RenovateNamespace = "renovate"
 	RenovateCron      = "renovate"
 	ManualCron        = "manual"
@@ -14,6 +16,14 @@ const (
 func Job() {
 	k := client.NewEnvironment()
 
+	trivy(k)
+
+	if false {
+		renovate(k)
+	}
+}
+
+func renovate(k *client.Client) {
 	for _, j := range k.CronJobs(RenovateNamespace) {
 		fmt.Printf("Delete: %s\n", j.Name)
 		k.DeleteJobWatch(RenovateNamespace, j.Name)
@@ -25,5 +35,20 @@ func Job() {
 
 	k.DeleteJobWatch(RenovateNamespace, ManualCron)
 	j := k.CreateJobFromCron(RenovateNamespace, RenovateCron, ManualCron)
+	fmt.Printf("Job: %s\n", j.Name)
+}
+
+func trivy(k *client.Client) {
+	for _, j := range k.CronJobs(TrivyNamespace) {
+		fmt.Printf("Delete: %s\n", j.Name)
+		k.DeleteJobWatch(TrivyNamespace, j.Name)
+	}
+
+	if t := k.CronJob(TrivyNamespace, "missing"); t != nil {
+		fmt.Printf("Job: %s\n", t.Name)
+	}
+
+	k.DeleteJobWatch(TrivyNamespace, ManualCron)
+	j := k.CreateJobFromCron(TrivyNamespace, TrivyCron, ManualCron)
 	fmt.Printf("Job: %s\n", j.Name)
 }

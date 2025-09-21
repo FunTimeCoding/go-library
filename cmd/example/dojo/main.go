081199f2578b3597423a926329f37ebef6ff6c5d
Alexander Reitzel <funtimecoding@gmail.com>
2025-09-21 16:42:10 +0200

    remove comments

--- cmd/example/dojo/main.go
+++ cmd/example/dojo/main.go
@@ -3,10 +3,5 @@ package main
 import "github.com/funtimecoding/go-library/pkg/defect_dojo"
 
 func main() {
-	// https://github.com/truemilk/go-defectdojo
-	//  Fork: https://github.com/blackaichi/go-defectdojo
-	// https://github.com/egongu90/defectdojo-go
-	// Make own openapi-generated client or build basic client?
-	//  https://docs.defectdojo.com/en/api/api-v2-docs/
 	defect_dojo.NewEnvironment()
 }

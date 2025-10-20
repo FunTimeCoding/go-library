3b9d0578db12c676ed9ace339e1b7518e9e6401a
Alexander Reitzel <funtimecoding@gmail.com>
2025-10-20 16:58:21 +0200

    sanitize

--- pkg/tool/goclean/clean/run.go
+++ pkg/tool/goclean/clean/run.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 	"github.com/funtimecoding/go-library/pkg/git/remote/provider_map"
 	"github.com/funtimecoding/go-library/pkg/tool/goclean/clean/option"
+	web "github.com/funtimecoding/go-library/pkg/web/constant"
+	"strings"
 )
 
 func Run(o *option.Clean) {
+	o.GitLabHost = strings.TrimPrefix(o.GitLabHost, web.SecurePrefix)
 	m := providerMap(o)
 	r := originRemote(o, m)
 

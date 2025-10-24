49967ba5dd3a88cb1c9ee93d4b62b54e3e70d7ee
Alexander Reitzel <funtimecoding@gmail.com>
2025-10-24 19:10:21 +0200

    its only the reference if the reference itself is not main

--- pkg/git/branch_test.go
+++ pkg/git/branch_test.go
@@ -4,16 +4,22 @@ import (
 	"github.com/funtimecoding/go-library/pkg/assert"
 	"github.com/funtimecoding/go-library/pkg/git/constant"
 	"github.com/funtimecoding/go-library/pkg/github/action"
+	github "github.com/funtimecoding/go-library/pkg/github/constant"
 	"github.com/funtimecoding/go-library/pkg/system"
+	"github.com/funtimecoding/go-library/pkg/system/environment"
 	"testing"
 )
 
 func TestBranch(t *testing.T) {
 	system.PrintEnvironment()
-	var e string
+	e := constant.HeadReference
 
 	if action.IsActionRun() {
-		e = constant.HeadReference
+		if r := environment.Required(
+			github.ReferenceEnvironment,
+		); r != constant.MainBranch {
+			e = r
+		}
 	} else {
 		e = constant.MainBranch
 	}

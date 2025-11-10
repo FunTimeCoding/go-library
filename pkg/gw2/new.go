655d8c3814bcf7c00bbf3bbae8e797143b675383
Alexander Reitzel <funtimecoding@gmail.com>
2025-11-10 22:27:43 +0100

    documentation link

--- pkg/gw2/new.go
+++ pkg/gw2/new.go
@@ -3,6 +3,10 @@ package gw2
 import "github.com/MrGunflame/gw2api"
 
 func New(token string) *Client {
+	// https://wiki.guildwars2.com/wiki/API:Main
+	// Alternate:
+	//  https://github.com/yasvisu/gw2api
+	//  https://github.com/42atomys/gw2api-go
 	client := gw2api.New()
 
 	if token != "" {

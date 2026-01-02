93df33cbdb77235160599f9923cfa09aaf5cdf11
Alexander Reitzel <funtimecoding@gmail.com>
2026-01-02 10:08:57 +0100

    strip protocol prefix

--- pkg/prometheus/alertmanager/alert/full_host.go
+++ pkg/prometheus/alertmanager/alert/full_host.go
@@ -3,11 +3,13 @@ package alert
 import (
 	"github.com/funtimecoding/go-library/pkg/network"
 	"github.com/funtimecoding/go-library/pkg/prometheus/constant"
+	web "github.com/funtimecoding/go-library/pkg/web/constant"
 	"strings"
 )
 
 func (a *Alert) FullHost() string {
 	result := a.Detail(constant.InstanceLabel)
+	result = strings.TrimPrefix(result, web.SecurePrefix)
 
 	if strings.ContainsRune(result, ':') {
 		return network.SplitHost(result)

6ff1a293355c96a45a9fd9b4c2c7b4e7f983f3f1
Alexander Reitzel <funtimecoding@gmail.com>
2026-01-02 14:18:45 +0100

    upload closes it

--- pkg/web/post_file.go
+++ pkg/web/post_file.go
@@ -1,7 +1,6 @@
 package web
 
 import (
-	"fmt"
 	"github.com/funtimecoding/go-library/pkg/errors"
 	"github.com/funtimecoding/go-library/pkg/system"
 	"github.com/funtimecoding/go-library/pkg/web/constant"
@@ -14,21 +13,14 @@ func PostFile(
 	user string,
 	password string,
 ) {
+	// HTTP upload closes it
 	f := system.Open(name)
-	defer func() {
-		fmt.Println("Close file")
-		errors.PanicClose(f)
-	}()
-
 	r := NewPostBytes(url, f)
 	r.SetBasicAuth(user, password)
 	r.Header.Set(constant.ContentType, constant.Form)
 	r.ContentLength = system.FileStat(f).Size()
 	s := Send(Client(), r)
-	defer func() {
-		fmt.Println("Close response body")
-		errors.PanicClose(s.Body)
-	}()
+	defer errors.PanicClose(s.Body)
 
 	if !ResponseOkay(s) {
 		log.Panicf("upload %d: %s", s.StatusCode, ReadString(s))

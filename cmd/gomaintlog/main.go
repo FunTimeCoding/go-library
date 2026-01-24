033becfa06c408d7a062aba32ac52994a716a9b0
Alexander Reitzel <funtimecoding@gmail.com>
2026-01-24 17:20:32 +0100

    reuse server wrapper

--- cmd/gomaintlog/main.go
+++ cmd/gomaintlog/main.go
@@ -1,26 +1,14 @@
 package main
 
 import (
-	"context"
+	"github.com/funtimecoding/go-library/pkg/generative/model_context/server"
 	"github.com/funtimecoding/go-library/pkg/maintenance_log"
 	"github.com/funtimecoding/go-library/pkg/system"
-	"github.com/funtimecoding/go-library/pkg/web"
-	"github.com/funtimecoding/go-library/pkg/web/constant"
-	"github.com/mark3labs/mcp-go/server"
-	"log"
 )
 
 func main() {
-	a := constant.Listen
-	log.Printf("Listen %s/mcp", a)
-	s := web.Server(
-		server.NewStreamableHTTPServer(
-			maintenance_log.New().Nested(),
-			server.WithStateful(true),
-		),
-		a,
-	)
-	web.ServeAsynchronous(s)
+	s := server.New(maintenance_log.New().Nested())
+	s.Serve()
 	system.KillSignalBlock()
-	web.GracefulShutdown(context.Background(), s, false)
+	s.Stop()
 }

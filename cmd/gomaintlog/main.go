83cc36c29f814cd90fc8284fac5f51b592539e94
Alexander Reitzel <funtimecoding@gmail.com>
2026-02-20 10:36:49 +0100

    dictionary, import alias

--- cmd/gomaintlog/main.go
+++ cmd/gomaintlog/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"context"
-	mcpServer "github.com/funtimecoding/go-library/pkg/generative/model_context/server"
+	"github.com/funtimecoding/go-library/pkg/generative/model_context/server"
 	"github.com/funtimecoding/go-library/pkg/maintenance_log"
 	"github.com/funtimecoding/go-library/pkg/system"
 	"github.com/funtimecoding/go-library/pkg/web"
@@ -10,10 +10,10 @@ import (
 )
 
 func main() {
-	s := mcpServer.New(maintenance_log.New().Nested())
+	s := server.New(maintenance_log.New().Nested())
 	m := http.NewServeMux()
 	s.Setup(m)
-	h := web.Server(m, mcpServer.Address)
+	h := web.Server(m, server.Address)
 	web.ServeAsynchronous(h)
 	system.KillSignalBlock()
 	web.GracefulShutdown(context.Background(), h, true)

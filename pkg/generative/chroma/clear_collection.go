7436de5d9c01cb7b3c235efadff18e7a433fc661
Alexander Reitzel <funtimecoding@gmail.com>
2026-02-24 13:58:02 +0100

    update chroma

--- pkg/generative/chroma/clear_collection.go
+++ pkg/generative/chroma/clear_collection.go
@@ -4,5 +4,5 @@ import "github.com/amikos-tech/chroma-go/pkg/api/v2"
 
 func (c *Client) ClearCollection(l v2.Collection) {
 	// Deletes all, even if no name field exists
-	c.Delete(l, v2.WithWhereDelete(v2.NotEqString("name", "")))
+	c.Delete(l, v2.WithWhere(v2.NotEqString("name", "")))
 }
--- pkg/generative/chroma/delete_identifiers.go
+++ pkg/generative/chroma/delete_identifiers.go
@@ -6,5 +6,5 @@ func (c *Client) DeleteIdentifiers(
 	l v2.Collection,
 	i ...v2.DocumentID,
 ) {
-	c.Delete(l, v2.WithIDsDelete(i...))
+	c.Delete(l, v2.WithIDs(i...))
 }

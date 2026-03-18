cbd7e81b4ed8bc172e28216ec9066e5421a20efc
Alexander Reitzel <funtimecoding@gmail.com>
2026-03-18 17:52:08 +0100

    constant

--- pkg/system/macos/example/bundle.go
+++ pkg/system/macos/example/bundle.go
@@ -2,13 +2,14 @@ package example
 
 import (
 	"github.com/funtimecoding/go-library/pkg/generative/model_context/constant"
+	system "github.com/funtimecoding/go-library/pkg/system/constant"
 	"github.com/funtimecoding/go-library/pkg/system/macos"
 )
 
 func Bundle() {
 	macos.CreateBundle(
 		"Example",
-		"tmp",
+		system.Temporary,
 		"tmp/example",
 		"tmp/icon.icns",
 		"sh.s3n",

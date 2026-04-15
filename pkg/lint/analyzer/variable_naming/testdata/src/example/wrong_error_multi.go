81139d664c961316c3a7b65acc91b44431c8a543
Alexander Reitzel <funtimecoding@gmail.com>
2026-04-15 19:35:47 +0200

    different name so golint doesnt flag it

--- pkg/lint/analyzer/variable_naming/testdata/src/example/wrong_error_multi.go
+++ pkg/lint/analyzer/variable_naming/testdata/src/example/wrong_error_multi.go
@@ -3,6 +3,6 @@ package example
 import "fmt"
 
 func WrongErrorMulti() {
-	err := fmt.Errorf("test") // want `variable err of type error should be named e`
-	_ = err
+	fail := fmt.Errorf("test") // want `variable fail of type error should be named e`
+	_ = fail
 }

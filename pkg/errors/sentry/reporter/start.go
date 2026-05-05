cfb9978cb576fc293706cedb847a3c3106df618d
Alexander Reitzel <funtimecoding@gmail.com>
2026-05-06 00:11:44 +0200

    do not return nil

--- pkg/errors/sentry/reporter/start.go
+++ pkg/errors/sentry/reporter/start.go
@@ -4,7 +4,7 @@ import "github.com/funtimecoding/go-library/pkg/errors/sentry"
 
 func (r *Reporter) Start() *Reporter {
 	if r.locator == "" {
-		return nil
+		return r
 	}
 
 	r.hub = sentry.Start(r.project, r.locator, r.version)

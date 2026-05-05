652438cc494a7a1ddc8ea9f2c317733299bd5d5f
Alexander Reitzel <funtimecoding@gmail.com>
2026-05-05 14:48:24 +0200

    event payload

--- pkg/errors/sentry/basic/response/event_entry_payload.go
+++ pkg/errors/sentry/basic/response/event_entry_payload.go
@@ -1,7 +1,16 @@
 package response
 
+import "encoding/json"
+
 type EventEntryPayload struct {
-	Values    []ExceptionValue `json:"values"`
-	Message   string           `json:"message"`
-	Formatted string           `json:"formatted"`
+	Values      []ExceptionValue  `json:"values"`
+	Message     string            `json:"message"`
+	Formatted   string            `json:"formatted"`
+	Locator     string            `json:"url"`
+	Method      string            `json:"method"`
+	Payload     json.RawMessage   `json:"data"`
+	Headers     [][]string        `json:"headers"`
+	Query       string            `json:"query"`
+	Cookies     [][]string        `json:"cookies"`
+	Environment map[string]string `json:"env"`
 }
